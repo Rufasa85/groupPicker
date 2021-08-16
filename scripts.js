4582b3aabec123711650db4f4bb976da0e5cdb3d
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-08-16 09:33:42 -0700

    adding stuff

--- scripts.js
+++ scripts.js
@@ -1,140 +1,149 @@
-let groupsDiv = document.querySelector('#groupsFlex');
-let randomBtn = document.querySelector('#randomGroups');
+let groupsDiv = document.querySelector("#groupsFlex");
+let randomBtn = document.querySelector("#randomGroups");
 const students = [
-    "Adam Levine",
-    "Ahmed Al-karzoun",
-    "Alex Milroy",
-    "Alexander Sun",
-    "Andrew Crow",
-    "Ann Guinee",
-    "Brandon Phillips",
-    "Caitlin Bouroncle",
-    "Chris Asmar",
-    "Chris Sison",
-    "Christopher  MacGeorge",
-    "Christopher  Wesonga",
-    "Daniel Gentile",
-    "Daniel Yoder",
-    "Darrian Coleman",
-    "David Guthmann",
-    "Devin Gillogy",
-    "Dexter Sage",
-    "Doug Mcntosh",
-    "Elijah Blaisdell",
-    "Ellie Fu",
-    "Emily Goeres",
-    "Eric Rosario",
-    "Evan Kirkland",
-    "Francine Babauta",
-    "Gabriel Rodrguez",
-    "Gavin Calkins",
-    "Hao Guan",
-    "Hilary Valencia-Walsh",
-    "Issac Walls",
-    "Ismahan  Jamea",
-    "Jack Solaro",
-    "Jacob Cowan",
-    "Jae Kim",
-    "Jae Mclain",
-    "Janelle Dean",
-    "Jonathan Kemp",
-    "Jose Morales",
-    "Joshua Jainga",
-    "Katie Dickson",
-    "Kayla Newton",
-    "Larry Cessna",
-    "Louis Longo",
-    "Maged Abdelsalam",
-    "Magnus Appel",
-    "Maria Waslick",
-    "Matthew Taylor",
-    "Matthew  Weber",
-    "Maxwell Hanson",
-    "Mercury Springberry",
-    "Mikael Fallesen",
-    "Nathan Brown",
-    "Neil Comisioneru",
-    "Nicholas Van Baak",
-    "Patrick Ceriale",
-    "Petar Zivkovic",
-    "Quint Turner",
-    "Robert Dalton",
-    "Samuel Clitty",
-    "Scott Dancer",
-    "Sean Morgan",
-    "Shunpin Tseng",
-    "Sophia Jung",
-    "Steven Bong",
-    "Thomas An",
-    "Timothy Phillips",
-    "Wesley Mcmillan",
-    "Zhouyi Wang",
-]
+  // "AC Kimari",
+  // "Albert Armas",
+  // "Charles Glass",
+  // "Christian Marano",
+  // "Connor Olson",
+  // "Darren Thamtoro",
+  // "David Lucio",
+  // "Derek DeLizo",
+  // "Dru Ludwig",
+  // "Erik Lew",
+  // "Jack Bentsen",
+  // "Jared Rose",
+  // "Jeff Gilbert",
+  // "Jose Benicio",
+  // "Jun (Kevin) Choi",
+  // "Kaleb Muse",
+  // "Karen Pion",
+  // "Keith Murphy",
+  // "Kestle Olson",
+  // "Kopi Kolyvek",
+  // "Leone Soth",
+  // "Lewis Capers",
+  // "Marcos Trejo",
+  // "Meghan Andrewartha",
+  // "Mike Lowther",
+  // "Nathaniel Turcotte",
+  // "Ran Gamboa",
+  // "Simran Kaur",
+  // "Tino Ganacias",
+  // "Vincent Dang",
+  // "Zach Maurer"
+];
+const spicyStudents = [
+  "AC Kimari",
+  "David Lucio",
+  "Derek DeLizo",
+  "Dru Ludwig",
+  "Erik Lew",
+  "Jack Bentsen",
+  "Jared Rose",
+  "Jun (Kevin) Choi",
+  "Kaleb Muse",
+  "Karen Pion",
+  "Keith Murphy",
+  "Koppi Kolyvek",
+  "Leone Soth",
+  "Marcos Trejo",
+  "Meghan Andrewartha",
+  "Mike Lowther",
+  "Nathaniel Turcotte",
+  "Ran Gamboa",
+  "Simran Kaur",
+  "Zach Maurer"
+];
+const mediumStudents = [
+  "Connor Olson",
+  "Charles Glass",
+  "Christian Marano",
+  "Darren Thamtoro",
+  "Lewis Capers",
+];
+
+const mildStudents = [
+   "Tino Ganacias",
+  ];
+
 //function that takes a randomized array and returns it shuffled
 function shuffleArray(arr) {
-    let arrCopy = arr.slice();
-    let shuffled = [];
-    while (arrCopy.length > 0) {
-        let randomStudent = arrCopy.splice(Math.floor(Math.random() * arrCopy.length), 1)[0];
-        // console.log(randomStudent);
-        shuffled.push(randomStudent);
-    }
-    return shuffled;
+  let arrCopy = arr.slice();
+  let shuffled = [];
+  while (arrCopy.length > 0) {
+    let randomStudent = arrCopy.splice(
+      Math.floor(Math.random() * arrCopy.length),
+      1
+    )[0];
+    // console.log(randomStudent);
+    shuffled.push(randomStudent);
+  }
+  return shuffled;
 }
 
-//function that takes in array of names and group size, returns groups maxed at that size.  
+//function that takes in array of names and group size, returns groups maxed at that size.
 //TODO: rewrite groupMaker to append to one array at a time, ensure more even size dist for uneven numbers
 function groupMaker(arr, size) {
-    let groups = [];
-    // set up group buckets based on number of students
-    for (let i = 0; i < Math.ceil(arr.length / size); i++) {
-        groups.push([]);
-    }
-    // console.log(groups);
-    // let currentGroup = [];
-    // let numGroups = Math.ceil(arr.length/s ize);
-    // console.log('total groups:', numGroups)
-    arr.forEach((student, idx) => {
-        console.log((idx + 1) % groups.length);
-        groups[(idx + 1) % groups.length].push(student)
-        // currentGroup.push(student);
-        // console.log(student)
-        // if(currentGroup.length===size){
-        //     groups.push(currentGroup);
-        //     currentGroup = [];
-        // }
-    })
-    //hack for uneven final group
-    // if(currentGroup.length > 0) {
+  if(arr.length===size+1){
+    return [arr]
+  }
+  let groups = [];
+  // set up group buckets based on number of students
+  for (let i = 0; i < Math.ceil(arr.length / size); i++) {
+    groups.push([]);
+  }
+  // console.log(groups);
+  // let currentGroup = [];
+  // let numGroups = Math.ceil(arr.length/s ize);
+  // console.log('total groups:', numGroups)
+  arr.forEach((student, idx) => {
+    console.log((idx + 1) % groups.length);
+    groups[(idx + 1) % groups.length].push(student);
+    // currentGroup.push(student);
+    // console.log(student)
+    // if(currentGroup.length===size){
     //     groups.push(currentGroup);
+    //     currentGroup = [];
     // }
-    return groups;
+  });
+  //hack for uneven final group
+  // if(currentGroup.length > 0) {
+  //     groups.push(currentGroup);
+  // }
+  return groups;
 }
 
 //function to update view with groups
-function updateView(twoDimensionalArray) {
-    let groupCount = 1;
-    twoDimensionalArray.forEach(array => {
-        let thisGroup = document.createElement('div');
-        thisGroup.classList.add('groupDiv')
-        groupsDiv.appendChild(thisGroup);
-        thisGroup.innerHTML = `<ol></ol>`
-        groupCount++;
-        array.forEach(student => {
-            thisGroup.querySelector('ol').innerHTML += `<li>${student}</li>`
-        })
-    })
+function updateView(twoDimensionalArray, level) {
+  let groupCount = 1;
+  twoDimensionalArray.forEach(array => {
+    let thisGroup = document.createElement("div");
+    thisGroup.classList.add("groupDiv");
+    thisGroup.classList.add(level);
+    groupsDiv.appendChild(thisGroup);
+    thisGroup.innerHTML = `<h2>${level} group ${groupCount}</h2><ol></ol>`;
+    groupCount++;
+    array.forEach(student => {
+      thisGroup.querySelector("ol").innerHTML += `<li>${student}</li>`;
+    });
+  });
 }
 
 //handle random button click
-randomBtn.addEventListener('click', e => {
-    let randomGroups = groupMaker(shuffleArray(students), 5);
-    e.target.classList.add('hidden');
-    updateView(randomGroups);
-})
-
-
+randomBtn.addEventListener("click", e => {
+  let randomSpicyGroups = groupMaker(shuffleArray(spicyStudents), 4);
+  let randomMedGroups = groupMaker(shuffleArray(mediumStudents), 4);
+  e.target.classList.add("hidden");
+  updateView(randomSpicyGroups, "spicy");
+  updateView(randomMedGroups, "medium");
+  mildStudents.length ? updateView([mildStudents], "mild") : null;
+});
 
 //TODO: float list of students, allow selectable absentees
 
 //TODO: ask user for group size, adujust accordingly
+
+document.addEventListener("click", ({ target }) => {
+  console.log(target);
+});
